615aa6d347a58b721e9f540f953780b4c8f2ed06
wuhanqing <554553400@qq.com>
2022-01-24 11:37:58 +0800

    translate shefit reviews

--- outputs/shefit.py
+++ outputs/shefit.py
@@ -2,6 +2,7 @@ from pyscrapy.models import Goods, GoodsReview
 from outputs.baseoutput import BaseOutput
 import json
 from translate import Translator
+import requests
 
 
 class ShefitOutput(BaseOutput):
@@ -15,7 +16,11 @@ class ShefitOutput(BaseOutput):
         self.translator = Translator(to_lang='chinese', provider='mymemory')  # , proxies={'http': '127.0.0.1:1080'}
 
     def to_chinese(self, content: str):
-        return self.translator.translate(content)
+        # return self.translator.translate(content)
+        url = f"http://fanyi.youdao.com/translate?&doctype=json&type=AUTO&i={content}"
+        response = requests.get(url)
+        json_data = response.json()
+        return json_data["translateResult"][0][0]["tgt"]
 
     def output(self):
         sheet = self.work_sheet
@@ -47,34 +52,52 @@ class ShefitOutput(BaseOutput):
             # 返回商品信息递增列 next col index
             self.set_values_to_row(sheet, goods_info_list, goods_row_index, goods_col_index)
             goods_row_index += 1
+        self.save_reviews()
         self.wb.save(self.output_file)
 
-    def test(self):
-        reviews = self.db_session.query(GoodsReview).filter(Goods.site_id == self.site_id).all()
-        trans_map = {}
+    def update_reviews_rows(self, reviews_rows_per: list, reviews_rows: list):
+        reviews_rows_per_str = json.dumps(reviews_rows_per)
+        reviews_rows_per_str_cn = self.to_chinese(reviews_rows_per_str)
+        print(reviews_rows_per_str_cn)
+        reviews_rows_per = json.loads(reviews_rows_per_str_cn)
+        for review_row in reviews_rows_per:
+            reviews_rows.append(review_row)
+
+    def save_reviews(self):
+        sheet_reviews = self.wb.create_sheet(title='评论详情', index=1)
+        details_title_row = ('时间', '星级', '体型', '年龄', 'activity', '标题', '详情')
+        sheet_reviews.append(details_title_row)
+        reviews = self.db_session.query(GoodsReview).filter(GoodsReview.site_id == self.site_id).all()
+        reviews_rows = []
+        reviews_rows_per = []
         times = 0
         for review in reviews:
-            if times > 0:
-                break
-
-            code = review.code
+            times += 1
+            # code = review.code
             body = review.body
             title = review.title
+            # title = self.to_chinese(title)
+            review.title = title
             body_type = review.body_type
             activity = review.activity
+            rating = review.rating_value
             age = review.age
+            time_text = review.time_str
+            review_row = (time_text, rating, body_type, age, activity, title, body)
+            # sheet_reviews.append(review_row)
+            # self.db_session.commit()
+            reviews_rows_per.append(review_row)
+            if times % 10 == 0:
+                self.update_reviews_rows(reviews_rows_per, reviews_rows)
+                reviews_rows_per = []
+            if times == len(reviews):
+                self.update_reviews_rows(reviews_rows_per, reviews_rows)
 
-            if code not in trans_map:
-                trans_map[code] = dict(title=title, body=body, activity=activity, body_type=body_type)
-        print(trans_map)
-        # ak = json.dumps(trans_map)
-        # sk = self.to_chinese(ak)
-        # print(sk)
-        # print(json.loads(sk))
+        for review_row in reviews_rows:
+            sheet_reviews.append(review_row)
 
 
 if __name__ == '__main__':
     ot = ShefitOutput()
-    # ot.output()
-    ot.test()
-
+    ot.output()
+    # print(ot.to_chinese("Running/walking/hiking, HIIT/Weightlifting/Gym, Yoga/Low intensity , Everyday Use, Other"))
---
+++ service/Translator.py
@@ -0,0 +1,65 @@
+import sys
+import uuid
+import requests
+import hashlib
+import time
+from imp import reload
+
+reload(sys)
+
+YOUDAO_URL = 'https://httpbin.org/get'
+APP_KEY = '12345'
+APP_SECRET = '6789'
+
+
+def encrypt(signStr):
+    hash_algorithm = hashlib.sha256()
+    hash_algorithm.update(signStr.encode('utf-8'))
+    return hash_algorithm.hexdigest()
+
+
+def truncate(q):
+    if q is None:
+        return None
+    size = len(q)
+    return q if size <= 20 else q[0:10] + str(size) + q[size - 10:size]
+
+
+def do_request(data):
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    return requests.post(YOUDAO_URL, data=data, headers=headers)
+
+
+def connect():
+    q = "Running/walking/hiking, HIIT/Weightlifting/Gym, Yoga/Low intensity , Everyday Use, Other"
+
+    data = {}
+    data['from'] = 'en'
+    data['to'] = 'zh'
+    data['signType'] = 'v3'
+    curtime = str(int(time.time()))
+    data['curtime'] = curtime
+    salt = str(uuid.uuid1())
+    signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET
+    sign = encrypt(signStr)
+    data['appKey'] = APP_KEY
+    data['q'] = q
+    data['salt'] = salt
+    data['sign'] = sign
+    # data['vocabId'] = "您的用户词表ID"
+
+    response = do_request(data)
+    contentType = response.headers['Content-Type']
+    if contentType == "audio/mp3":
+        millis = int(round(time.time() * 1000))
+        filePath = "合成的音频存储路径" + str(millis) + ".mp3"
+        fo = open(filePath, 'wb')
+        fo.write(response.content)
+        fo.close()
+    else:
+        print(response.content)
+
+
+if __name__ == '__main__':
+    connect()
+
